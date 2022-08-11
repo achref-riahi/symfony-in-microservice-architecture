624a8191cbbb3056e5c2eae970b2282e9508cbe6
Achref RIAHI <riahi_achref@hotmail.fr>
2022-08-11 03:04:59 +0100

    fix(inventory): :construction: add BaldinofRoadRunnerBundle to kernel manualy

--- inventory/config/bundles.php
+++ inventory/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
 ];
